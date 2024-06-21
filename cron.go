a6c2791806fa8026c7ef3e042c36586703289eba
zhihanz <zzh243402448@gmail.com>
2024-06-21 11:25:47 +0800

    chore: fix blocking select

--- cron.go
+++ cron.go
@@ -3,6 +3,7 @@
 package cron
 
 import (
+	"github.com/sirupsen/logrus"
 	"sort"
 	"time"
 )
@@ -231,8 +232,12 @@ func (c *Cron) run() {
 				e := runEarlyEntry.CurrentEntry
 				e.Prev = runEarlyEntry.EndTime
 				e.Next = e.Schedule.Next(runEarlyEntry.EndTime)
+				logrus.WithField("name", runEarlyEntry.CurrentEntry.Name).WithField("prev", runEarlyEntry.CurrentEntry.Prev).WithField("next", runEarlyEntry.CurrentEntry.Next).Infof("Start Run early entry")
 				go c.execute(e)
+				logrus.WithField("name", runEarlyEntry.CurrentEntry.Name).WithField("prev", runEarlyEntry.CurrentEntry.Prev).WithField("next", runEarlyEntry.CurrentEntry.Next).Infof("End Run early entry")
+
 			}
+			continue
 		case now = <-time.After(effective.Sub(now)):
 			// Run every entry whose next time was this effective time.
 			for _, e := range c.entries {
@@ -241,7 +246,9 @@ func (c *Cron) run() {
 				}
 				e.Prev = e.Next
 				e.Next = e.Schedule.Next(effective)
+				logrus.WithField("name", e.Name).WithField("prev", e.Prev).WithField("next", e.Next).Infof("Start Run entry")
 				go c.execute(e)
+				logrus.WithField("name", e.Name).WithField("prev", e.Prev).WithField("next", e.Next).Infof("End Run entry")
 			}
 			continue
 
@@ -267,6 +274,8 @@ func (c *Cron) run() {
 
 		case <-c.stop:
 			return
+		default:
+			// avoid bloc
 		}
 
 		// 'now' should be updated after newEntry and snapshot cases.
